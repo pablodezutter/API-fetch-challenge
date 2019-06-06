ddb4d12498f578d92dca4559df98331f751cbae0
awvdeker <alexander.vandekerckhove@gmail.com>
2019-06-06 11:02:09 +0200

    25 cards of beers

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ return data;
 
 }
 
-// console.log 
+// console.log
 allBeerCall().then(data => {
-    console.log(data); 
-    console.log(data[0].name); 
+  console.log(data);
+  console.log(data[0].name);
+
+  var cardHolder = document.getElementById('cardHolder');
+
+  for (let i = 0 ; i < data.length ; i++){
+    cardHolder.innerHTML += `
+      <div class="card" style="width: 18rem;">
+        <img src="${data[i].image_url}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${data[i].name}</h5>
+          <p class="card-text">${data[i].tagline}</p>
+          <a href="#" class="btn btn-primary">Details</a>
+        </div>
+      </div>
+    `;
+  }
 })
