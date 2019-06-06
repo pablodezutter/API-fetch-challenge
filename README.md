383374e300dc0ebc0adb1e0832c69c3f8deb9bfe
pablodezutter <dezutterpablo@gmail.com>
2019-06-06 14:13:33 +0200

    commit conditions, images kleiner, innerHTML is empty

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # API-fetch-challenge
 Donderdag 6 juni, coop with Alex 
 #Create files, setup branch
+#create functions to fetch data
+##style the index with bootstrap
+##make a loop for showing the cards
+##show the cards in script.js with `
+#buttons creeëren, javascript eventlisteners opzetten
+if condition toevoegen voor currentpage  1
+fetch url aangepast, en 
+images verkleind
+ cardHolder.innerHTML="";
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //call the fetch function
 
-async function allBeerCall(){
-let result= await fetch( `https://api.punkapi.com/v2/beers`);
+async function allBeerCall(page){
+let result= await fetch( `https://api.punkapi.com/v2/beers?page=${page}&per_page=25`);
 
 let data= await result.json();
 return data;
@@ -9,16 +9,16 @@ return data;
 }
 
 // console.log
-allBeerCall().then(data => {
+allBeerCall(1).then(data => {
   console.log(data);
   console.log(data[0].name);
-
+console.log(data.length);
   var cardHolder = document.getElementById('cardHolder');
-
+    cardHolder.innerHTML="";
   for (let i = 0 ; i < data.length ; i++){
     cardHolder.innerHTML += `
       <div class="card m-4" style="width: 18rem;">
-        <img src="${data[i].image_url}" class="card-img-top">
+        <img src="${data[i].image_url}" class="card-img-top bottleImage">
         <div class="card-body">
           <h5 class="card-title">${data[i].name}</h5>
           <p class="card-text">${data[i].tagline}</p>
@@ -28,4 +28,38 @@ allBeerCall().then(data => {
     `;
   }
 })
+
+var currentPage= 1;
+var previousButton= document.getElementById("previousButton");
+var nextButton= document.getElementById("nextButton");
+var randomButton=document.getElementById("randomButton");
+
+nextButton.addEventListener("click", function(){
+        if (currentPage < 13){
+            
+            currentPage= currentPage +1;
+            allBeerCall(currentPage).then(data => {
+                              
+                var cardHolder = document.getElementById('cardHolder');
+                cardHolder.innerHTML= "";
+
+                for (let i = 0 ; i < data.length ; i++){
+                    
+                    cardHolder.innerHTML += `
+                    <div class="card m-4" style="width: 18rem;">
+                      <img src="${data[i].image_url}" class="card-img-top bottleImage">
+                      <div class="card-body">
+                        <h5 class="card-title">${data[i].name}</h5>
+                        <p class="card-text">${data[i].tagline}</p>
+                        <a href="#" class="btn btn-primary">Details</a>
+                      </div>
+                    </div>
+                  `;
+                }
+              })
+        }
+
+
+});
+
 
